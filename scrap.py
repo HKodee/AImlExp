75c5ccb314219b6833601fa78ebba43d99028097
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-06-16 20:11:09 +0530

    header dict for fruther use

--- scrap.py
+++ scrap.py
@@ -6,5 +6,13 @@ from bs4 import BeautifulSoup
 # Response 403
 
 
-requests.get('https://www.ambitionbox.com/list-of-companies?page=1').text
+#requests.get('https://www.ambitionbox.com/list-of-companies?page=1').text
 #can deny access
+
+# Define your headers dictionary
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://www.google.com/"
+}
+webpage = requests.get('https://www.ambitionbox.com/list-of-companies?page=1', headers=headers).text

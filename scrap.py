a7b0988cdf6d8a84648eb3993a3c1d60a4c8a234
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-06-16 20:05:18 +0530

    may deny access

--- scrap.py
+++ scrap.py
@@ -4,3 +4,7 @@ from bs4 import BeautifulSoup
 
 #requests.get('https://www.ambitionbox.com/list-of-companies?page=1')
 # Response 403
+
+
+requests.get('https://www.ambitionbox.com/list-of-companies?page=1').text
+#can deny access

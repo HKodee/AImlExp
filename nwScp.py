0de69b73d2e5a6acea0eefb265587f0178a8e82a
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-06-22 20:01:28 +0530

    loop created

--- nwScp.py
+++ nwScp.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+
+final = pd.DataFrame()
+
+for j in range(1,11):
+
+    url = 'https://www.ambitionbox.com/list-of-companies?page={}'.format()
+    
